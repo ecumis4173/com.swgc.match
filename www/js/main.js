e7dc22cb4fd95c83bc080e4135e79282a5945ceb
nathan <orange@oranges-Mac-mini.local>
2014-09-28 22:01:55 -0400

    first stab at sub pages

--- www/js/main.js
+++ www/js/main.js
@@ -20,8 +20,26 @@ function getEvents(){
                     for (var i=0; i<result.rows.length; i++){ 
                         var row=result.rows.item(i);
                         //alert(" " + row['event_id']+" " + row['event_name']+" " + row['date']+" ");
-                        var stringout = "<div onClick=goToPage('event.html')>" + row['event_id']+" " + row['event_name']+" " + row['date'] + "</div>"; 
+                        var stringout = "<div onClick=goToPage('event.html?id="+row['event_id']+"')>" + row['event_id']+" " + row['event_name']+" " + row['date'] + "</div>"; 
                         document.getElementById("eventList").innerHTML = document.getElementById("eventList").innerHTML +stringout;
+                    } 
+                }            
+            , errorCB);
+        });
+}
+function getParticipants(){
+    var sql = "SELECT * FROM participant_event pe JOIN participant p ON p.pid=pe.pid ORDER BY p.name";
+    var result;
+    db = window.openDatabase("swgc", "1.0", "swgc", 1000000);
+    db.transaction(
+        function (tx,errorCB){
+            tx.executeSql(sql, [], 
+                function (tx, result){
+                    for (var i=0; i<result.rows.length; i++){ 
+                        var row=result.rows.item(i);
+                        //alert(" " + row['event_id']+" " + row['event_name']+" " + row['date']+" ");
+                        var stringout = "<div onClick=goToPage('registration.html?id="+row['pe_id']+"')>" + row['name']+"</div>"; 
+                        document.getElementById("participantEventList").innerHTML = document.getElementById("participantEventList").innerHTML +stringout;
                     } 
                 }            
             , errorCB);
@@ -33,4 +51,14 @@ function queryDB(tx,errorCB){
 
 function debugAlert(txt){
     alert(txt);
+}
+function getId() {
+    var $_GET = {};
+    document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+        function decode(s) {
+            return decodeURIComponent(s.split("+").join(" "));
+        }
+        $_GET[decode(arguments[1])] = decode(arguments[2]);
+    });
+   return $_GET["id"];
 }
